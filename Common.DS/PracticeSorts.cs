6e0b1139d6a01df6db642f55d1be6ccd66395c1d
aaronn718 <aaronn718@gmail.com>
2024-06-06 10:37:19 -0700

    practice

---
+++ Common.DS/PracticeSorts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Common.DS
+{
+    
+/*
+    A place to practice sorts, doesn't need to be saved
+*/
+    public class PracticeSorts : Sorts
+    {
+
+        protected override void DoSort(int[] myArray)
+        {
+            return;
+        }
+        public override string PrettyName()
+        {
+            return "Practice Sort";
+        }
+    }
+
+}
