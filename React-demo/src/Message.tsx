dc3e365404251a6d6b59ea9b22e1de8d453ab538
Aaron Nguyen <aaronn718@gmail.com>
2024-06-13 09:33:14 -0700

    add node stuffs

---
+++ React-demo/src/Message.tsx
@@ -0,0 +1,15 @@
+//pascalCasing
+function Message(){
+    //JSX: Javascript xml
+    //<h1> is a component that can be converted to javascript - babel created it
+    const name = "Aaron";
+    return <h1>Hello {name}</h1>;
+}
+/* 
+function Banner(someTitle)
+{
+    return <h2>someTitle</h2>;
+} */
+
+export default Message;
+//export default Banner;
