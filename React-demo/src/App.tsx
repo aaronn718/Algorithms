c0015dd7b0ddf821ae1fc838d35ede198a1c8034
aaronn718 <aaronn718@gmail.com>
2024-07-05 12:56:42 -0700

    api call react

--- React-demo/src/App.tsx
+++ React-demo/src/App.tsx
@@ -1,22 +1,31 @@
 import ListGroup from "./components/ListGroup";
+import DataFetcher from './components/DataFetcher';
 
 function App(){
-    let props = {
-      items : ["Washington",
-      "California",
-      "Texas",
-      "Florida",
-      "MainItem"], 
-      heading : "heading from main"
-    };
+  //   let props = {
+  //     items : ["Washington",
+  //     "California",
+  //     "Texas",
+  //     "Florida",
+  //     "MainItem"], 
+  //     heading : "heading from main"
+  //   };
     
-    const handleSelectedItem = function(item: string){
-      console.log(item);
-    }
+  //   const handleSelectedItem = function(item: string){
+  //     console.log(item);
+  //   }
 
-  return <> <div><ListGroup items={props.items} heading={props.heading} onSelectItem={handleSelectedItem} ></ListGroup></div>
-  <div><ListGroup items={["Blue", "Red", "Yellow"]} heading="list 2"  onSelectItem={handleSelectedItem}></ListGroup></div>
-  </>;
+  // return <> <div><ListGroup items={props.items} heading={props.heading} onSelectItem={handleSelectedItem} ></ListGroup></div>
+  // <div><ListGroup items={["Blue", "Red", "Yellow"]} heading="list 2"  onSelectItem={handleSelectedItem}></ListGroup></div>
+  // </>;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DataFetcher />
+      </header>
+    </div>
+  );
 }
 
 export default App;
